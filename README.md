804d78dad6d49063ff37d97cc42fa2dd1679e0d7
Luis Ruediger <luisruediger27@gmail.com>
2022-12-19 14:15:58 -0300

    day 03

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-- Nest
-- Prisma
+# Notification Service 
+
+Microserviço de notificações, desenvolvido durante o evento Ignite Lab <br/>
+
+---
+
+## Funcionalidades
+
+- [x] Criar notificação
+- [x] Cancelar notificação
+- [x] Contar notiticações
+- [x] Notificações visualizadas / Não visualizadas
+- [x] Testes com Jest
+
+---
+
+## Tecnologias
+
+- [`Node`](https://nodejs.org/en/)
+- [`Nest`](https://nestjs.com/)
+- [`Jest`](https://jestjs.io/pt-BR/)
+- [`Prisma`](https://www.prisma.io/)
--- src/application/use-cases/send-notification.ts
+++ src/application/use-cases/send-notification.ts
@@ -3,7 +3,7 @@ import { Content } from '../entities/content';
 import { Notification } from '../entities/notification';
 import { NotificationsRepository } from '../repositories/notification-repository';
 
-interface sendNotificationRequest {
+interface SendNotificationRequest {
   recipientId: string;
   content: string;
   category: string;
@@ -18,7 +18,7 @@ export class SendNotification {
   constructor(private notificationsRepository: NotificationsRepository) {}
 
   async execute(
-    request: sendNotificationRequest,
+    request: SendNotificationRequest,
   ): Promise<SendNotificationResponse> {
     const { recipientId, content, category } = request;
 
